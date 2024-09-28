0dabed94264ff7df0dce04272b733fb64eb27b54
cyrusjamesanghay <cj.anghay@gmail.com>
2024-09-28 11:00:27 +0800

    mycopytask

---
+++ mycopytask1.py
@@ -0,0 +1,17 @@
+age = [-12,13,-14,15,-16]
+username = ["juan", "pedro", "james", "sagaad", "fuego",]
+for dataage in age:
+    if dataage > 0:
+        print (str(dataage) + "negative")
+    elif dataage < 0:
+        print (str(dataage) + "postive")
+
+username = ["juan", "pedro", "james", "saggad", "fuego"]
+for dataUser in username:
+    if dataUser == "james":
+        print("james cute")
+    else:    
+        print("james none")        
+
+
+
